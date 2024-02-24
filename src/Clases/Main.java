a0edfa57a33cb47cf26804b3d8c1287e9663c481
SebasAure <saure@correo.unimet.edu.ve>
2024-02-24 19:19:28 -0400

    base de la interfaz lista

---
+++ src/Clases/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import Interfaces.VentanaSimulacion;
+
+/**
+ *
+ * @author sebas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaSimulacion simulacion = new VentanaSimulacion();
+        simulacion.setVisible(true);
+        simulacion.setLocationRelativeTo(null);
+    }
+    
+}
